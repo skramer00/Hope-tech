60fed744067c9eb76e5808e880c691025a042c8e
Scott Kramer <scott.skweb@gmail.com>
2026-08-02 22:58:03 -0700

    Add Hope Tech Assistant page

---
+++ src/app/ask/page.tsx
@@ -0,0 +1,8 @@
+import HopeTechAssistant from "@/components/HopeTechAssistant";
+import "./assistant.css";
+
+export default function AskHopeTechPage() {
+  return <main className="wrap section">
+    <HopeTechAssistant />
+  </main>;
+}
