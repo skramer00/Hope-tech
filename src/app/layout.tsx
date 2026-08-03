76a72d9250f04e213c01ffe098ea0670de18e08c
Scott Kramer <scott.skweb@gmail.com>
2026-08-03 13:28:45 -0700

    Load persistent support button styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import Link from "next/link";
 import "./globals.css";
+import "./support.css";
 
 export const metadata: Metadata = {
   title: "Hope Technical Ministries",
